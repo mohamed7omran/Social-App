912c6b71511c1f7bd15363630c644fdf5e68378a
abdullah <abdullahomrana12312332110h@gmail.com>
2024-06-14 03:10:10 +0300

    ubdate the style

--- my-app/src/components/friendsPage/friends.jsx
+++ my-app/src/components/friendsPage/friends.jsx
@@ -3,31 +3,72 @@ import Image from "next/image";
 
 const Friends = () => {
   return (
-    <div className=" bg-gray-900 h-80 w-1/5 pt-5 mt-5">
-      <h1 className="ml-5 mb-3">Friend List</h1>
-      <div className="flex mb-3 items-center ">
-        <div className="image  ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
-          <Image
-            src="/image/avatar.png"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
+    <div className="  h-80 w-1/4 pt-5 mt-5">
+      <div className="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex items-center justify-between mb-4">
+          <h5 className="text-xl font-bold leading-none text-gray-900 dark:text-white">
+            Friend List
+          </h5>
+          <a
+            href="#"
+            className="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+          >
+            View all
+          </a>
         </div>
-        <h2>M.Omran</h2>
-        <div className="flex grow justify-end mr-3">+</div>
-      </div>
-      <div className="flex mb-3 items-center ">
-        <div className="image  ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
-          <Image
-            src="/image/avatar.png"
-            alt="Picture of the author"
-            width={500}
-            height={500}
-          />
+        <div className="flow-root">
+          <ul
+            role="list"
+            className="divide-y divide-gray-200 dark:divide-gray-700"
+          >
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center">
+                <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+                  <Image
+                    src="/image/avatar.png"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Neil Sims
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  +
+                </div>
+              </div>
+            </li>
+            <li className="py-3 sm:py-4">
+              <div className="flex items-center ">
+                <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+                  <Image
+                    src="/image/avatar.png"
+                    alt="Picture of the author"
+                    width={500}
+                    height={500}
+                  />
+                </div>
+                <div className="flex-1 min-w-0 ms-4">
+                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                    Bonnie Green
+                  </p>
+                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    email@windster.com
+                  </p>
+                </div>
+                <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                  +
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <h2>M.Omran</h2>
-        <div className="flex grow justify-end mr-3">+</div>
       </div>
     </div>
   );
--- my-app/src/components/scroll/scroll.jsx
+++ my-app/src/components/scroll/scroll.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "next/image";
-import styles from "./scroll.css";
 const Scroll = () => {
   return (
     <div className="  w-2/4 pt-5 px-5 h-screen">
@@ -17,30 +16,117 @@ const Scroll = () => {
         <h1 className=" ml-2 bg-blue-700 rounded-full w-8 h-8 flex justify-center items-center ">
           +
         </h1>
+        <div className="flex grow justify-end mr-3">
+          <button
+            type="button"
+            className="duration-500 text-xs text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg px-3 py-2 text-center me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800"
+          >
+            Sign Up
+          </button>
+          <button
+            type="button"
+            className="duration-500 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-xm px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+          >
+            Login
+          </button>
+        </div>
       </div>
 
-      <div className="bg-gray-900">
-        <div className="mt-5 pt-5 ">
-          <div className="flex items-center ">
-            <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+      <div className="posts">
+        <div className=" post bg-gray-900 pb-3 rounded-lg">
+          <div className="mt-5 pt-5 ">
+            <div className="flex items-center ">
+              <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+                <Image
+                  src="/image/avatar.png"
+                  alt="Picture of the author"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <h2>M.Omran</h2>
+              <div className="flex grow justify-end mr-3">+</div>
+            </div>
+            <p className="ml-5 mt-4">Hello I'm mohamed omran</p>
+            <div className=" px-5 mt-2 ">
               <Image
-                src="/image/avatar.png"
+                src="/image/photo.png"
                 alt="Picture of the author"
-                width={500}
+                width={1000}
                 height={500}
               />
             </div>
-            <h2>M.Omran</h2>
-            <div className="flex grow justify-end mr-3">+</div>
+            <div className="pl-5">
+              <h2 className="text-xs text-slate-400">2 min ago</h2>
+              <div className="flex mt-2 ">
+                <div>Like 16</div>
+                <div className="ml-3">comm 5</div>
+              </div>
+            </div>
           </div>
-          <p className="ml-5 mt-4">Hello I'm mohamed omran</p>
-          <div className=" px-5 mt-2 ">
-            <Image
-              src="/image/photo.png"
-              alt="Picture of the author"
-              width={1000}
-              height={500}
-            />
+        </div>
+        <div className="post bg-gray-900 pb-3 rounded-lg">
+          <div className="mt-5 pt-5 ">
+            <div className="flex items-center ">
+              <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+                <Image
+                  src="/image/avatar.png"
+                  alt="Picture of the author"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <h2>M.Omran</h2>
+              <div className="flex grow justify-end mr-3">+</div>
+            </div>
+            <p className="ml-5 mt-4">Hello I'm mohamed omran</p>
+            <div className=" px-5 mt-2 ">
+              <Image
+                src="/image/photo.png"
+                alt="Picture of the author"
+                width={1000}
+                height={500}
+              />
+            </div>
+            <div className="pl-5">
+              <h2 className="text-xs text-slate-400">2 min ago</h2>
+              <div className="flex mt-2 ">
+                <div>Like 16</div>
+                <div className="ml-3">comm 5</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="post bg-gray-900 pb-3 rounded-lg">
+          <div className="mt-5 pt-5 ">
+            <div className="flex items-center ">
+              <div className="image ml-5 mr-3 rounded-full w-12 h-12 flex justify-center items-center ">
+                <Image
+                  src="/image/avatar.png"
+                  alt="Picture of the author"
+                  width={500}
+                  height={500}
+                />
+              </div>
+              <h2>M.Omran</h2>
+              <div className="flex grow justify-end mr-3">+</div>
+            </div>
+            <p className="ml-5 mt-4">Hello I'm mohamed omran</p>
+            <div className=" px-5 mt-2 ">
+              <Image
+                src="/image/photo.png"
+                alt="Picture of the author"
+                width={1000}
+                height={500}
+              />
+            </div>
+            <div className="pl-5">
+              <h2 className="text-xs text-slate-400">2 min ago</h2>
+              <div className="flex mt-2 ">
+                <div>Like 16</div>
+                <div className="ml-3">comm 5</div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
